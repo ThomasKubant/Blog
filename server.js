a25720fd71299c8b3861dd9cbc5e6f28be7fac40
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-05-08 18:40:40 -0700

    Homepage complete

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
-
-
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
